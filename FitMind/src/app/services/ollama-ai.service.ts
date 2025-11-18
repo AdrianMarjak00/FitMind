b9d9edade8303c53bb8fab42cc3789e961382402
AdrianMarjak00 <adrianmarjak1@gmail.com>
2025-11-18 10:43:34 +0100

    ...

--- FitMind/src/app/services/ollama-ai.service.ts
+++ FitMind/src/app/services/ollama-ai.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { OllamaResponse } from '../models/ollama-response';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OllamaService {
 
-  private apiUrl = 'http://localhost:11434/api/generate'; // zmeň podľa potreby
+  constructor(private httpClient: HttpClient) { }
 
-  constructor(private http: HttpClient) {}
-
-  generateResponse(prompt: string): Observable<any> {
-    const body = {
-      model: 'llama3',         // alebo iný model
+  processPrompt(prompt: string): Observable<OllamaResponse> {
+    const requestBody = {
+      model: environment.llamaModel,
       prompt: prompt
     };
 
-    return this.http.post<any>(this.apiUrl, body);
+    return this.httpClient.post<OllamaResponse>(environment.llamaApiUrl, requestBody);
   }
 }
