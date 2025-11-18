02b6a8d156b66776acbaabbac154130b3b05343e
Tomas <tomas.sedlak.07@gmail.com>
2025-11-18 10:44:33 +0100

    generate comp

---
+++ FitMind/src/app/ollama-ai/ollama-ai.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ollama-ai',
+  imports: [],
+  templateUrl: './ollama-ai.html',
+  styleUrl: './ollama-ai.scss',
+})
+export class OllamaAi {
+
+}
