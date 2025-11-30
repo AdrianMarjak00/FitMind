d5d71a35ed7ee564db63d9d8be4bf84243cf139c
Tomas <tomas.sedlak.07@gmail.com>
2025-11-30 14:16:29 +0100

    piechart

--- FitMind/src/app/app.routes.ts
+++ FitMind/src/app/app.routes.ts
@@ -3,20 +3,16 @@ import { HomeComponent } from './home/home';
 import { Contact } from './contact/contact';
 import { RegisterComponent } from './register/register';
 import { LoginComponent } from './login/login';
-import { ReviewsComponent } from './reviews/reviews';
+import { PiechartComponent } from './piechart/piechart';
 
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'contact', component: Contact },
-    { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'review', component: ReviewsComponent },
-
-
-
-
-    // { path: '**', redirectTo: '' } // Voliteľné: presmerovanie na Domov pri neznámej ceste
+    { path: 'register', component: RegisterComponent },
+    { path: 'contact', component: Contact },
+    { path: 'piechart', component: PiechartComponent },
+    { path: 'review', component: PiechartComponent },
 
 ];
