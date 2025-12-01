dc95940993bde323c508d802d87e5c263b5aab1a
Tomas <tomas.sedlak.07@gmail.com>
2025-12-01 23:03:06 +0100

    priprava piechart

--- FitMind/src/app/app.routes.ts
+++ FitMind/src/app/app.routes.ts
@@ -1,20 +1,20 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home';
 import { Contact } from './contact/contact';
 import { RegisterComponent } from './register/register';
 import { LoginComponent } from './login/login';
-import { PiechartComponent } from './piechart/piechart';
 import { OllamaAi } from './ollama-ai/ollama-ai';
-
-
+import { Piechart } from './piechart/piechart';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'contact', component: Contact },
-    { path: 'piechart', component: PiechartComponent },
-    { path: 'review', component: PiechartComponent },
-     { path: 'ai-chat', component: OllamaAi }, 
+    { path: 'ai-chat', component: OllamaAi },
+    { path: 'piechart', component: Piechart },
 
+    // fallback
+    { path: '**', redirectTo: '' }
 ];
