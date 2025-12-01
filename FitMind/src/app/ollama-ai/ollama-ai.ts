1dfee83abc79962b65ac7f5a03dfeb213856d2e9
AdrianMarjak00 <adrianmarjak1@mgial.com>
2025-12-01 05:48:20 +0100

    ollama upgrade

--- FitMind/src/app/ollama-ai/ollama-ai.ts
+++ FitMind/src/app/ollama-ai/ollama-ai.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OllamaService } from '../services/ollama-ai.service';
+import { OllamaResponse } from '../models/ollama-response';
 
 @Component({
   selector: 'app-ollama-ai',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './ollama-ai.html',
-  styleUrl: './ollama-ai.scss',
+  styleUrls: ['./ollama-ai.scss'],
 })
 export class OllamaAi {
 
+  prompt: string = '';
+  response: string = '';
+  loading: boolean = false;
+
+  constructor(private ollamaService: OllamaService) {}
+
+  sendPrompt() {
+    if (!this.prompt.trim()) return;
+
+    this.loading = true;
+    this.response = '';
+
+    this.ollamaService.processPrompt(this.prompt).subscribe({
+      next: (res: OllamaResponse) => {
+        this.response = res.response;
+        this.loading = false;
+      },
+      error: (error) => {
+        this.response = 'Chyba: ' + error.message;
+        this.loading = false;
+      }
+    });
+  }
 }
