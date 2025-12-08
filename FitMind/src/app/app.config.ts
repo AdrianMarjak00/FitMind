c7971eb74e024e369939de25a35d00d44a0c5d8f
AdrianMarjak00 <adrianmarjak1@gmail.com>
2025-12-08 20:56:06 +0100

    ..

--- FitMind/src/app/app.config.ts
+++ FitMind/src/app/app.config.ts
@@ -4,9 +4,9 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyArvOFbqncllijGFJPoHNEgtPdZPIuCqjQ",
@@ -18,16 +18,16 @@ const firebaseConfig = {
     measurementId: "G-74VP65JX6H"
 };
 
-
-export const firebaseApp = initializeApp(firebaseConfig);
-export const firebaseAuth = getAuth(firebaseApp);
-export const firebaseDB = getFirestore(firebaseApp);
-
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideHttpClient(),
-        provideAnimations()
-    ],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+
+    // 🔥 Toto je to, čo ti chýbalo
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
 };
