348849bf1bc251f8e47307e36e29286fc3c35581
AdrianMarjak00 <adrianmarjak1@mgial.com>
2026-01-11 15:21:17 +0100

    .

--- FitMind/src/app/app.routes.ts
+++ FitMind/src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { HomeComponent } from './home/home';
 import { Contact } from './components/contact/contact';
 import { RegisterComponent } from './components/register/register';
 import { LoginComponent } from './components/login/login';
-import { AdminGuard } from '../guards/admin.guard';
-import { ReviewsComponent } from './reviews/reviews';
 import { Piechart } from './components/piechart/piechart';
+import { AiChatComponent } from './components/ai-chat/ai-chat';
+import { DashboardComponent } from './components/dashboard/dashboard';
+import { ReviewsComponent } from './reviews/reviews';
 import { Jedalnicek } from './components/jedalnicek/jedalnicek';
 import { Training } from './components/training/training';
-import { DashboardComponent } from './components/dashboard/dashboard';
-import { AiChatComponent } from './components/ai-chat/ai-chat';
+import { AdminGuard } from '../guards/admin.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -19,7 +19,7 @@ export const routes: Routes = [
     { path: 'contact', component: Contact },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'piechart', component: Piechart, canActivate: [AdminGuard] },
-    { path: 'review', component: ReviewsComponent },
+    { path: 'review', component: ReviewsComponent     },
     { path: 'jedalnicek', component: Jedalnicek },
     { path: 'training', component: Training },
     { path: 'ai-chat', component: AiChatComponent },
