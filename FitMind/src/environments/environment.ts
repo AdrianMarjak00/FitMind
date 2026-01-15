04374236c49ed032d09fbfc4c3416d17d471bdf8
AdrianMarjak00 <adrianmarjak1@mgial.com>
2026-01-15 23:41:23 +0100

    .

--- FitMind/src/environments/environment.ts
+++ FitMind/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fitmind-production-21d5.up.railway.app/api',
+  apiUrl: 'https://fitmind-dba6a.web.app/',
   llamaApiUrl: 'http://localhost:11434/api/generate',
   llamaModel: 'llama3.2:3b'
 };
