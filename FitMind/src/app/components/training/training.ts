76cdfe3c1e8f3f84afda6ad9912a2db554a94a03
Tomas <tomas.sedlak.07@gmail.com>
2026-01-15 21:23:48 +0100

    priprava na platobnu branu, dizajn dashboardu a cely dizajn hotovy

--- FitMind/src/app/components/training/training.ts
+++ FitMind/src/app/components/training/training.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-training',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
+  ],
   templateUrl: './training.html',
   styleUrl: './training.scss',
 })
 export class Training {
 
+  buyPlan(planType: string) {
+    console.log(`Inicializujem nákup pre plán: ${planType}`);
+    // Tu môžeš pridať navigáciu na platobnú bránu alebo Stripe
+    // Príklad: window.location.href = 'https://checkout.stripe.com/...';
+    alert(`Vybral si si ${planType.toUpperCase()} plán. Systém ťa teraz presmeruje na platobnú bránu.`);
+  }
 }
