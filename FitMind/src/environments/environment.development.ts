4233bfb100a2ceb8aa92eedfeb0657e004a51dfc
AdrianMarjak00 <adrianmarjak1@mgial.com>
2026-01-18 22:46:36 +0100

    .

--- FitMind/src/environments/environment.development.ts
+++ FitMind/src/environments/environment.development.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
   // Pridal som /api na koniec, ak tvoj backend tie cesty takto má
-  apiUrl: 'https://fitmind-production-21d5.up.railway.app/api',
+  apiUrl: 'https://fitmind-581538831484.europe-west1.run.app/api'
 
-  // TOTO JE PROBLÉM: localhost na webe nebude fungovať
-  llamaApiUrl: 'https://fitmind-production-21d5.up.railway.app/api/generate', // Skús to smerovať cez tvoj Railway backend
-  llamaModel: 'llama3.2:3b'
 };
