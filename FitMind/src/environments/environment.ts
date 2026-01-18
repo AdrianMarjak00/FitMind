9edc4de30d22b52a16bf281669ee8073f0c7bea6
AdrianMarjak00 <adrianmarjak1@mgial.com>
2026-01-18 22:32:53 +0100

    ...

--- FitMind/src/environments/environment.ts
+++ FitMind/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fitmind-production-21d5.up.railway.app/api' // Pridaj /api, ak tvoje cesty začínajú takto
+  apiUrl: 'https://fitmind-581538831484.europe-west1.run.app'
 };
